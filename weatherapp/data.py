db174d8847c686c13650204863e1994be2ca4607
daisys <daisyjnshih@gmail.com>
2014-03-01 01:30:10 -0800

    initial data model

---
+++ weatherapp/data.py
@@ -0,0 +1,63 @@
+__author__ = 'DaisyS'
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    clothingHistoryEntry = db.relationship('ClothingHistoryEntry', backref='user', lazy='dynamic')
+    autoCompleteGear = db.relationship('UserGear', backref='user_gear', lazy='dynamic')
+
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return '<User %d>' % self.id
+
+
+usergear = db.Table('usergear',
+                    db.Column('gear', db.String(100), primary_key=True),
+                    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                    db.Column('frequency', db.Integer))
+
+
+class ClothingHistoryEntry(db.Model):
+    location = db.Column(db.String(80), primary_key=True)
+    date = db.Column(db.Date, primary_key=True)
+    temperatures = db.Column(db.Integer)
+    precipitation = db.Column(db.Integer)
+    comfort = db.Column(db.String(80))
+    windSpeed = db.Column(db.Integer)
+    windGust = db.Column(db.Integer)
+    humidity = db.Column(db.Integer)
+    cloudPrecip = db.Column(db.String(80))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    gears = db.Column(db.String(100))
+
+    def __init__(self, temperatures, precipitation, comfort, windSpeed, windGust, humidity, cloudPrecip,
+                 user_id, gears):
+        self.temperatures = temperatures
+        self.precipitation = precipitation
+        self.comfort = comfort
+        self.windSpeed = windSpeed
+        self.windGust = windGust
+        self.humidity = humidity
+        self.cloudPrecip = cloudPrecip
+        self.gears = gears
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<ClothingHistoryEntry, user=%d, date=%s>' % (self.user_id, str(self.date))
+
+
+class DefaultGears(db.Model):
+    item = db.Column(db.String, primary_key=True)
+
+    def __init(self):
+        pass
+
+    def __repr__(self):
+        return '<DefaultGears %d>' % self.item
+
